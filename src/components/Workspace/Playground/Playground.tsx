f7227ea5d6831d9dd404f9b9381f895cce6593d8
liuguangsen0409 <865688540@qq.com>
2024-02-18 00:13:21 +0800

    feat: workspace page and local problem

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
 import EditorFooter from './EditorFooter/EditorFooter';
 import Split from 'react-split';
 import CodeMirror from '@uiw/react-codemirror';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { javascript } from '@codemirror/lang-javascript';
+import { Problem } from '@/utils/types/problem';
 
 type PlaygroundProps = {
-  
+  problem: Problem
 };
 
-const Playground:React.FC<PlaygroundProps> = () => {
-  const boilerPlate = `function twoSum(nums, target) {
-  // Write your code here
-}`
+const Playground:React.FC<PlaygroundProps> = ({ problem }) => {
+  const [activeTestCaseId, setActiveTestCaseId] = useState(0) 
   
   return (
     <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
@@ -21,7 +20,7 @@ const Playground:React.FC<PlaygroundProps> = () => {
       <Split className="h-[calc(100vh-146px)]" direction="vertical" sizes={[60, 40]} minSize={60}>
         <div className="w-full overflow-auto">
           <CodeMirror 
-            value={boilerPlate}
+            value={problem.starterCode}
             theme={vscodeDark}
             extensions={[javascript()]}
             style={{fontSize: 16}}
@@ -37,34 +36,29 @@ const Playground:React.FC<PlaygroundProps> = () => {
           </div>
 
           <div className="flex">
-            {/* case 1 */}
-            <div className="mr-2 items-start mt-2 text-white">
-              <div className="flex flex-wrap items-center gap-y-4">
-                <div className="caseBtn">Case 1</div>
-              </div>
-            </div>
-            {/* case 2 */}
-            <div className="mr-2 items-start mt-2 text-white">
-              <div className="flex flex-wrap items-center gap-y-4">
-                <div className="caseBtn">Case 2</div>
-              </div>
-            </div>
-            {/* case 3 */}
-            <div className="mr-2 items-start mt-2 text-white">
-              <div className="flex flex-wrap items-center gap-y-4">
-                <div className="caseBtn">Case 3</div>
-              </div>
-            </div>
+            {
+              problem.examples.map((example, index) => (
+                <div 
+                  className="mr-2 items-start mt-2 text-white" 
+                  key={example.id}
+                  onClick={() => setActiveTestCaseId(example.id)}
+                >
+                  <div className="flex flex-wrap items-center gap-y-4">
+                    <div className="caseBtn">Case {index + 1}</div>
+                  </div>
+                </div>
+              ))
+            }
           </div>
 
           <div className="font-semibold my-4">
             <p className="text-sm font-medium mt-4 text-white">Input:</p>
             <div className="caseText">
-              nums: [2, 7, 11, 15], target: 9
+              { problem.examples[activeTestCaseId].inputText }
             </div>
             <p className="text-sm font-medium mt-4 text-white">Output:</p>
             <div className="caseText">
-              [0, 1]
+              { problem.examples[activeTestCaseId].outputText }
             </div>
           </div>
         </div>
--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import Topbar from '@/components/Topbar/Topbar';
 import Workspace from '@/components/Workspace/Workspace';
+import { problems } from '@/utils/problems';
+import { Problem } from '@/utils/types/problem';
 
 type ProblemPageProps = {
-  
+  problem: Problem
 };
 
-const ProblemPage:React.FC<ProblemPageProps> = () => {
+const ProblemPage:React.FC<ProblemPageProps> = ({ problem }) => {
   return (
     <>
       <Topbar problemPage />
-      <Workspace />
+      <Workspace problem={problem} />
     </>
   )
 }
 export default ProblemPage;
+
+// fetch the local data
+// SSG static site generation
+// getStaticPaths => it create the dynamic routes
+export async function getStaticPaths () {
+  const paths = Object.keys(problems).map((key) => ({
+    params: {pid: key}
+  }))
+
+  return {
+    paths,
+    fallback: false // pid doesn't match jump to 404
+  }
+} 
+
+// getStaticProps => it fetch the data
+export async function getStaticProps ({ params }: { params: { pid: string } }) {
+  const { pid } = params
+  const problem = problems[pid]
+
+  if (!problem) {
+    return {
+      notFound: true
+    }
+  }
+
+  problem.handlerFunction = problem.handlerFunction.toString()
+
+  return {
+    props: {
+      problem
+    }
+  }
+}
